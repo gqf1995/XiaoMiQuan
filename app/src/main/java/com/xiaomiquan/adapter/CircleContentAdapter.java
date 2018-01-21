ed85a51e37a7b1fe8e28da954eb90ebdd555715d
谷乐园 <gly94@qq.com>
2018-01-21 16:21:07 +0800

    gly

--- app/src/main/java/com/xiaomiquan/adapter/CircleContentAdapter.java
+++ app/src/main/java/com/xiaomiquan/adapter/CircleContentAdapter.java
@@ -1,15 +1,25 @@
 package com.xiaomiquan.adapter;
 
 import android.content.Context;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
 
+import com.fivefivelike.mybaselibrary.utils.GsonUtil;
+import com.fivefivelike.mybaselibrary.utils.ToastUtil;
+import com.fivefivelike.mybaselibrary.utils.callback.DefaultClickLinsener;
 import com.fivefivelike.mybaselibrary.view.FontTextview;
 import com.xiaomiquan.R;
 import com.xiaomiquan.entity.bean.circle.CircleContent;
+import com.xiaomiquan.entity.bean.circle.Commend;
 import com.xiaomiquan.entity.bean.circle.UserTopic;
 import com.xiaomiquan.widget.circle.CommentListView;
 import com.xiaomiquan.widget.circle.PraiseListView;
 import com.zhy.adapter.recyclerview.CommonAdapter;
 import com.zhy.adapter.recyclerview.base.ViewHolder;
+
+import org.w3c.dom.Comment;
 
 import java.util.List;
 
@@ -25,26 +35,68 @@ public class CircleContentAdapter extends CommonAdapter<UserTopic>{
     FontTextview con_name;
     FontTextview con_time;
     FontTextview con_con;
-    PraiseListView praiseListView;
-    CommentListView commentList;
+    RecyclerView topic_commend;
+    TextView pinglun;
+    TextView zan;
+    TextView zan_num;
+    TextView pinglun_num;
 
+    List<Commend> commends;
+    Context con;
+    DefaultClickLinsener defaultClickLinsener;
+
+    public void setDefaultClickLinsener(DefaultClickLinsener defaultClickLinsener) {
+        this.defaultClickLinsener = defaultClickLinsener;
+    }
 
     public CircleContentAdapter(Context context, List<UserTopic> datas) {
         super(context,R.layout.adapter_circle_con,datas);
+        con=context;
     }
 
     @Override
-    protected void convert(ViewHolder holder, UserTopic circleContent, int position) {
+    protected void convert(ViewHolder holder, UserTopic circleContent, final int position) {
         con_head=holder.getView(R.id.con_head);
         con_name=holder.getView(R.id.con_name);
         con_time=holder.getView(R.id.con_time);
         con_con=holder.getView(R.id.con_con);
-        praiseListView=holder.getView(R.id.praiseListView);
-        commentList=holder.getView(R.id.commentList);
+        pinglun=holder.getView(R.id.pinglun);
+
+
+        zan=holder.getView(R.id.zan);
+        zan_num=holder.getView(R.id.zan_num);
+        pinglun_num=holder.getView(R.id.pinglun_num);
+        topic_commend=holder.getView(R.id.topic_commend);
 
         con_name.setText(circleContent.getNickName());
         con_time.setText(circleContent.getCreateTimeStr());
         con_con.setText(circleContent.getContent());
+        zan_num.setText(circleContent.getPraiseQty()+"");
+        pinglun_num.setText(circleContent.getCommentQty()+"");
+        if (circleContent.getCommentList()!=null){
+        commends = GsonUtil.getInstance().toList(circleContent.getCommentList(),Commend.class);
+        iniCommend(commends,topic_commend);
+        }
+        pinglun.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(defaultClickLinsener!=null){
+                    defaultClickLinsener.onClick(view,position,null);
+                }
+            }
+        });
+    }
+    public void iniCommend(List<Commend> commentlist,RecyclerView commendlist){
+        CommendAdapter commendAdapter=new CommendAdapter(con,commentlist);
+        commendlist.setLayoutManager(new LinearLayoutManager(con) {
+            @Override
+            public boolean canScrollVertically() {
+                return false;
+            }
+
+        });
+        commendlist.setAdapter(commendAdapter);
 
     }
+
 }
--- app/src/main/java/com/xiaomiquan/utils/UiHeplUtils.java
+++ app/src/main/java/com/xiaomiquan/utils/UiHeplUtils.java
@@ -720,4 +720,23 @@ public class UiHeplUtils {
                     + String.valueOf((size % 100)) + " GB";
         }
     }
+
+    /**
+     * gly 添加
+     */
+    public static int dip2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    public static int px2dip(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+    /** 获取手机的密度*/
+    public static float getDensity(Context context) {
+        DisplayMetrics dm = context.getResources().getDisplayMetrics();
+        return dm.density;
+    }
 }
